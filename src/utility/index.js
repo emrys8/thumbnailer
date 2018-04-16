cc08f44141118d2239260dada197f032db1f804a
Micah <mickey4sure2k7@gmail.com>
2018-04-16 23:31:30 +0100

    Fix typo

--- src/utility/index.js
+++ src/utility/index.js
@@ -3,19 +3,5 @@ export default (resizer) => {
 
     const createThumbnail = (imageUrl) => {
       // NOT IMPLEMENTED YET: Image Resizer service
-    },
-
-    const applyJsonPatch = (patcher, patch, document) => {
-      // NOT IMPLEMENTED YET
-      return new Promise((resolve, reject) => {
-        if (!document || !patch) {
-          reject(new Error('No document to patch, please provide a document'));
-        } else {
-          const patchedObject = patcher.apply_patch(document, patch);
-          resolve(patchedObject);
-        }
-      });
     }
-
-    return { createThumbnail, applyJsonPatch };
 }
