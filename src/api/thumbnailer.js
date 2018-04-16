b4c792407719cef67dfd6e12a0bcb9ef6f61fcf3
Micah <mickey4sure2k7@gmail.com>
2018-04-16 14:57:04 +0100

    Fix bug

--- src/api/thumbnailer.js
+++ src/api/thumbnailer.js
@@ -2,7 +2,7 @@ import apiHelper from './api_helpers';
 const { loginUser, createImageThumbnail, jsonPatch } = apiHelper;
 
 export default (app) => {
-    app.get('/login', loginUser);
-    app.get('/create-thumbnail', createImageThumbnail);
-    app.get('/apply-json-patch', jsonPatch);
+    app.post('/login', loginUser);
+    app.post('/create-thumbnail', createImageThumbnail);
+    app.post('/apply-json-patch', jsonPatch);
 }
