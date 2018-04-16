17a0d4504a018a8379e7aed27207acb0d74967eb
Micah <mickey4sure2k7@gmail.com>
2018-04-16 14:51:20 +0100

    Add new test cases

---
+++ src/test/thumbnail.js
@@ -0,0 +1,77 @@
+import { expect } from 'chai';
+import request from 'supertest';
+import server from '../server';
+import { userInfo } from 'os';
+
+describe('Thumbnail API', () => {
+    let app = null;
+    let userToken;
+    let testUser = {
+        username: 'dennis',
+        password: 'ritchie'
+    };
+
+    beforeEach(() => {
+        return server.start(3000)
+          .then(serv => {
+              app = serv;
+          });
+    });
+
+    afterEach(() => {
+        app.close();
+        app = null;
+    });
+
+    describe('User Mock Login', () => {
+        describe('handle invalid login request', () => {
+            it('should reject an invalid login request', (done) => {
+                request(app)
+                  .post('/api/login')
+                  .send({ username: '', password: '' })
+                  .expect((res) => {
+                      expect(res.statusCode).to.equal(400, 'Bad Request');
+                      expect(res.body.status).to.equals(false);
+                  })
+                  .expect(400, done);
+            });
+        });
+
+        describe('handle valid login request', () => {
+            it('should login a user with the right credentials', (done) => {
+                request(app)
+                  .post('/api/login')
+                  .send(testUser)
+                  .expect((res) => {
+                      userToken = res.body.token;
+                      expect(res.statusCode).to.be.equal(200);
+                      expect(res.body.status).to.equals(true);
+                      expect(typeof res.body.token).to.equals('string');
+                  })
+                  .expect(200, done);
+            });
+        });
+    });
+
+    describe('Thumbnail Creation', () => {
+        it('should reject request with malformed image URL', (done) => {
+            request(app)
+              .post('/api/create-thumbnail')
+              .set('x-access-token', userToken)
+              .send({ imageUrl: 'file://my-image.jpg' })
+              .expect((res) => {
+                  expect(res.statusCode).to.be.equal(400, 'Bad Request');
+                  expect(res.body.status).to.equals(false);
+              })
+              .expect(400, done);
+        });
+
+        it('should create an image thumbnail', (done) => {
+            request(app)
+             .post('/api/create-thumbnail')
+             .set('x-access-token', userToken)
+             .send({ imageUrl: 'http://my-image.jpg' })
+             .expect(200, done);
+        });
+    });
+})
